aff6daa0d110b8f98be3401846f292b0af648790
Manoj A <manubhat701@gmail.com>
2026-06-15 12:30:48 +0530

    sorted-IQ

--- Test/sorteddropdown.spec.ts
+++ Test/sorteddropdown.spec.ts
@@ -1,1 +1,24 @@
- 
+import { test, expect, Locator } from "@playwright/test";
+
+test("sorteddropdown", async ({ page }) => {
+  await page.goto(
+    "https://testautomationpractice.blogspot.com/2018/09/automation-form.html",
+    { waitUntil: "domcontentloaded" },
+  );
+
+  //   const dropdown_option: Locator = page.locator("#animals>option");
+  const dropdown_option: Locator = page.locator("#colors>option");
+  //   console.log(await dropdown_option.allTextContents());
+
+  const opt_text: string[] = (await dropdown_option.allTextContents()).map(
+    (text) => text.trim(),
+  );
+
+  const original_arry: String[] = [...opt_text];
+  const sorted_list: string[] = [...opt_text].sort();
+
+  console.log("original_arry", original_arry);
+  console.log("sorted_list", sorted_list);
+
+  expect(original_arry).toEqual(sorted_list);
+});
